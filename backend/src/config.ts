9f502fd00d1b38632a5b11f2b3785a4be1e538ab
Margo <m.yunanova@gmail.com>
2023-11-30 19:50:06 +0300

    fix mongo host

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,4 @@
 import "dotenv/config";
 
 export const { JWT_SECRET = "JWT_SECRET" } = process.env;
-export const { DB_ADDRESS = "mongodb://localhost:27017/mestodb" } = process.env;
+export const { DB_ADDRESS = "mongodb://127.0.0.1:27017/mestodb" } = process.env;
